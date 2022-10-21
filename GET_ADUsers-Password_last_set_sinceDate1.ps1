a3bb24b34dac964fbb2746aef09889187b9aa2c4
Juan Botes <52822072+botesjuan@users.noreply.github.com>
2022-10-21 16:44:15 +0200

    Update GET_ADUsers-Password_last_set_sinceDate1.ps1

--- GET_ADUsers-Password_last_set_sinceDate1.ps1
+++ GET_ADUsers-Password_last_set_sinceDate1.ps1
@@ -1,12 +1,20 @@
-$searchdate = "2019-05-01" #yyyy-MM-dd format  
-$ADtopDN = "DC=top,DC=corp,DC=xo,DC=ya"  #  Active Directory search DN object top of domain
-  
-$PwdLastSetDate = $([datetime]::parseexact($searchdate,'yyyy-MM-dd',$null)).ToFileTime()  
-write-verbose "Finding users whose passwords have not changed since $([datetime]::fromfiletimeUTC($PwdLastSetDate))"  
-  
-$NoPassChangeData = Get-ADUser -filter { Enabled -eq $True } –Properties pwdLastSet -ADtopDN $ADtopDN |
-    where { $_.pwdLastSet -lt $PwdLastSetDate -and $_.pwdLastSet -ne 0 } |
-    Select-Object name,sAmAccountName,@{Name="PasswordLastSet";Expression={[datetime]::FromFileTimeUTC($_.pwdLastSet) } }
+# Nyumber of days in past to check from date before if account last logon and password last set date is less
+$Days = 365
+$searchdate = (Get-Date).Adddays(-($Days))
+$searchdate
 
-$NoPassChangeData.count
-$NoPassChangeData | Export-Csv -NoTypeInformation -Path "D:\output\NoPassChangeData.csv"
+$DormantLogons = Get-ADUser -Filter {(Enabled -eq $True) -and (LastLogonTimeStamp -lt $searchdate)} -Properties * |
+    Select Name,LastLogonDate,Enabled,SamAccountName,@{Name="PasswordLastSet";Expression={[datetime]::FromFileTimeUTC($_.pwdLastSet)}}
+
+# could have done oneliner - argh but i like peanuts
+$NotableAccounts = $DormantLogons | where { $_.PasswordLastSet -lt $searchdate -and $_.PasswordLastSet -ne 0 }
+
+# number of accounts that last logon date is less than date in past
+$DormantLogons.count
+# number of AD accounts that password last set is less than date in past
+$NotableAccounts.count
+
+# random account to check because the answer to the universe is 42
+$NotableAccounts[42]
+$NotableAccounts | Export-Csv D:\Scripts\output\Inactive_ADuserAccounts.csv -NoTypeInformation
+ 
